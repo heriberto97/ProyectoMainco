37bc693be9881f71888960b9c2408b43f2febf00
luisaguirre <luis_aguirre_1994@outlook.com>
2018-05-10 15:50:42 -0500

    hice funcionar la listview

--- src/sample/Controladores/Trabajadores.java
+++ src/sample/Controladores/Trabajadores.java
@@ -1,7 +1,16 @@
 package sample.Controladores;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TitledPane;
+import javafx.scene.input.ContextMenuEvent;
+import javafx.scene.input.MouseEvent;
+
+import javax.swing.text.TableView;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Trabajadores {
     @FXML
@@ -10,7 +19,27 @@ public class Trabajadores {
     TitledPane panel_faltas;
     @FXML
     TitledPane panel_prestamos;
+    @FXML
+    ListView<String> listview_trabajadores = new ListView<>();
+
+    ObservableList<String> list;
+    public void click_trabajador(MouseEvent mouseEvent) {
+        List<String> nombres= new ArrayList<>();
+
+        nombres.add("juan");
+        nombres.add("jose");
+        nombres.add("pedro");
+        nombres.add("antonio");
+
+        System.out.println("se seleccionó trabajadores");
+            list= FXCollections.observableList(nombres);
+        listview_trabajadores.setItems(list);
 
 
+    }
 
+    public void click_faltas(MouseEvent mouseEvent) {
+        System.out.printf("se seleccionó faltas");
+
+    }
 }
