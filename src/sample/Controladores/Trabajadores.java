e6f208403f629ea66b2dbd9b383b5c55c064bef1
luisaguirre <luis_aguirre_1994@outlook.com>
2018-05-10 14:52:28 -0500

    hago un listview funcionar

--- src/sample/Controladores/Trabajadores.java
+++ src/sample/Controladores/Trabajadores.java
@@ -1,4 +1,16 @@
 package sample.Controladores;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TitledPane;
+
 public class Trabajadores {
+    @FXML
+    TitledPane panel_trabajadores;
+    @FXML
+    TitledPane panel_faltas;
+    @FXML
+    TitledPane panel_prestamos;
+
+
+
 }
