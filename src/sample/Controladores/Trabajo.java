78bdb8426e75c2a5b5e98c060e86f113651c5d52
Ramirogag <sonexfkdx@hotmail.com>
2018-05-10 15:45:20 -0500

    Agregue Vista Trabajo/Trabajos

---
+++ src/sample/Controladores/Trabajo.java
@@ -0,0 +1,29 @@
+package sample.Controladores;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextArea;
+
+public class Trabajo {
+    @FXML
+    Button btn_nuevocliente;
+    @FXML
+    Button btn_ordendecompra;
+    @FXML
+    Button btn_esquema;
+    @FXML
+    Button btn_trabajadores;
+    @FXML
+    RadioButton rbtn_fabricacion;
+    @FXML
+    RadioButton rbtn_servicio;
+    @FXML
+    RadioButton rbtn_modificacion;
+    @FXML
+    ListView lv_clientes;
+    @FXML
+    TextArea txta_descripcion;
+
+}
--- src/sample/Controladores/Trabajos.java
+++ src/sample/Controladores/Trabajos.java
@@ -1,4 +1,22 @@
 package sample.Controladores;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+
 public class Trabajos {
+
+    @FXML
+    ListView lv_trabajosincompletos;
+    @FXML
+    ListView lv_trabajoscompletos;
+    @FXML
+    ComboBox cb_ordentrabajos;
+    @FXML
+    ComboBox cb_empresas;
+    @FXML
+    CheckBox checkb_urgente;
+
+
 }
