422845a7d4631cb5415580576bc580149339957c
angeloctaviopunk4 <octavioangelgg4@gmail.com>
2018-05-11 12:47:27 -0500

    Octa.....boton entrar abre ventana nueva

--- src/sample/Controladores/Login.java
+++ src/sample/Controladores/Login.java
@@ -2,6 +2,7 @@ package sample.Controladores;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -9,23 +10,37 @@ import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-public class Login  {
+public class Login implements Initializable {
     @FXML
     Button entrar;
-
-
-    public void click_entrar(MouseEvent mouseEvent)
+    @FXML
+    void iniciar(javafx.event.ActionEvent event)
     {
+        try
+        {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../Trabajadores.fxml"));
+            Parent abrir =fxmlLoader.load();
+            Stage s = new Stage();
+            s.setTitle("Trabajadores");
+            s.setScene(new Scene(abrir));
+            s.show();
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+    }
 
 
-
-
-
-
-        System.out.println("El octa chido");
-
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
 
     }
 }
