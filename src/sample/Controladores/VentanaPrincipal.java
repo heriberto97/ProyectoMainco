b88d0aef6d6873e963cd7b87d37b63461809e284
Oscar <oscar.olveda@live.com>
2018-05-14 12:26:14 -0500

    Actualización acciones de los botones principales

--- src/sample/Controladores/VentanaPrincipal.java
+++ src/sample/Controladores/VentanaPrincipal.java
@@ -46,4 +46,40 @@ public class VentanaPrincipal {
             System.out.println(e);
         }
     }
+
+    @FXML
+    void iniciar_empleados(javafx.event.ActionEvent event)
+    {
+        try
+        {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../Trabajadores.fxml"));
+            Parent abrir = fxmlLoader.load();
+            Stage s = new Stage();
+            s.setTitle("Maquinados industriales - Compras");
+            s.setScene(new Scene(abrir));
+            s.show();
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+    }
+
+    @FXML
+    void iniciar_inventario(javafx.event.ActionEvent event)
+    {
+        try
+        {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../Compras.fxml"));
+            Parent abrir = fxmlLoader.load();
+            Stage s = new Stage();
+            s.setTitle("Maquinados industriales - Compras");
+            s.setScene(new Scene(abrir));
+            s.show();
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+    }
 }
