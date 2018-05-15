1a0617f0e6b72ca135d6def676d36caff55f850c
luisaguirre <luis_aguirre_1994@outlook.com>
2018-05-15 09:28:53 -0500

    trabajo en la tabla trabajador

--- src/sample/Controladores/Trabajadores.java
+++ src/sample/Controladores/Trabajadores.java
@@ -17,8 +17,7 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import sample.objetos.Trabajador;
-
-import javax.swing.text.TableView;
+import javafx.scene.control.TableView;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -35,12 +34,31 @@ public class Trabajadores implements Initializable {
     @FXML
     ListView<String> listview_trabajadores = new ListView<>();
     @FXML
-    javafx.scene.control.TableView<Trabajador> table_trabajador;
+    TableView<Trabajador> table_trabajador= new TableView<>();
     private Stage stage;
     ObservableList<String> list;
 
-    public void click_trabajador(MouseEvent mouseEvent) {
+        public void click_trabajador(MouseEvent mouseEvent) {
 
+            }
+
+    public  ObservableList<Trabajador> getTrabajos(){
+        ObservableList<Trabajador> trabajadores= FXCollections.observableArrayList();
+        trabajadores.add(new Trabajador(1,"Antonio"));
+        trabajadores.add(new Trabajador(2,"Juan"));
+        trabajadores.add(new Trabajador(3,"Felipe"));
+        trabajadores.add(new Trabajador(4,"Ricardo"));
+        trabajadores.add(new Trabajador(5,"Luis"));
+        trabajadores.add(new Trabajador(6,"Pedro"));
+        return trabajadores;
+    }
+
+    public void click_faltas(MouseEvent mouseEvent) {
+        System.out.printf("se seleccionó faltas");
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
         List<String> nombres= new ArrayList<>();
         nombres.add("juan");
         nombres.add("jose");
@@ -51,38 +69,16 @@ public class Trabajadores implements Initializable {
         list= FXCollections.observableList(nombres);
         listview_trabajadores.setItems(list);
 
-        //columnas
-        TableColumn<Trabajador,Integer> id_Columna =new TableColumn<>("id");
-        id_Columna.setMinWidth(200);
-        id_Columna.setCellValueFactory(new PropertyValueFactory<>("id"));
-        //columnas
-        TableColumn<Trabajador,String> nombre_Columna =new TableColumn<>("nombre");
-        nombre_Columna.setMinWidth(200);
-        nombre_Columna.setCellValueFactory(new PropertyValueFactory<>("nombre"));
-
-        table_trabajador= new javafx.scene.control.TableView<>();
+        TableColumn firstNameCol = new TableColumn("Id");
+        TableColumn lastNameCol = new TableColumn("Nombre");
+        firstNameCol.setCellValueFactory(
+                new PropertyValueFactory<Trabajador,String>("Id")
+        );
+        lastNameCol.setCellValueFactory(
+                new PropertyValueFactory<Trabajador,String>("Nombre")
+        );
+        table_trabajador.getColumns().addAll(firstNameCol,lastNameCol);
         table_trabajador.setItems(getTrabajos());
-        table_trabajador.getColumns().addAll(id_Columna,nombre_Columna);
-
-    }
-
-    public  ObservableList<Trabajador> getTrabajos(){
-        ObservableList<Trabajador> trabajores= FXCollections.observableArrayList();
-        trabajores.add(new Trabajador(1,"Antonio"));
-        trabajores.add(new Trabajador(2,"Juan"));
-        trabajores.add(new Trabajador(3,"Felipe"));
-        trabajores.add(new Trabajador(4,"Ricardo"));
-        trabajores.add(new Trabajador(5,"Luis"));
-        trabajores.add(new Trabajador(6,"Pedro"));
-        return trabajores;
-    }
-
-    public void click_faltas(MouseEvent mouseEvent) {
-        System.out.printf("se seleccionó faltas");
-    }
-
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
 
     }
 
