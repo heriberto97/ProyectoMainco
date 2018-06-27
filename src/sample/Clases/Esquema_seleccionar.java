32afc251e725c4cb49ed529b9684f1d2a6b22d90
angeloctaviopunk4 <angeloctaviogg4@gmail.com>
2018-06-26 21:33:03 -0500

    esquemas

---
+++ src/sample/Clases/Esquema_seleccionar.java
@@ -0,0 +1,4 @@
+package sample.Clases;
+
+public class Esquema_seleccionar {
+}
--- src/sample/objetos/Esquema.java
+++ src/sample/objetos/Esquema.java
@@ -5,6 +5,7 @@ public class Esquema {
     int id;
     String ruta;
     String descripcion;
+    String numero;
 
     public Esquema(String ruta, String descripcion) {
         this.ruta = ruta;
@@ -15,6 +16,21 @@ public class Esquema {
         this.id = id;
         this.ruta = ruta;
         this.descripcion = descripcion;
+    }
+
+    public Esquema(int id, String ruta, String descripcion, String numero) {
+        this.id = id;
+        this.ruta = ruta;
+        this.descripcion = descripcion;
+        this.numero = numero;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
     }
 
     public int getId() {
