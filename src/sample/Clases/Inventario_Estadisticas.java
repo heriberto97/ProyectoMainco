bd5226f63e9d55c9148dd590a30e5f2649ce2016
angeloctaviopunk4 <angeloctaviogg4@gmail.com>
2018-07-18 23:27:48 -0500

    Cambios inventario estadisticas

---
+++ src/sample/Clases/Inventario_Estadisticas.java
@@ -0,0 +1,17 @@
+package sample.Clases;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Inventario_Estadisticas implements Initializable {
+    @FXML
+    Button btn_estadisticas_productos,btn_estadisticas_oficina;
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
